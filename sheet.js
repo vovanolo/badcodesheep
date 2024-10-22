3492ef1770c922fbbe92008cb8765c7b684ec2c5
Volodymyr Pankiv <39496114+vovanolo@users.noreply.github.com>
2024-10-22 12:28:30 +0300

    Create sheet.js

---
+++ sheet.js
@@ -0,0 +1,3 @@
+function worldChampionOfBadCode() { return `Final Absurd Result: ${((new Array(parseInt("one hundred".replace("one hundred", "100"))).fill(0).map((_, i) => Math.tan(i) * Math.random() * (new Date().getMilliseconds() + 1)).reduce((acc, val) => acc + Math.abs(val), 0).toString().split('').map(char => char.charCodeAt(0) % 100).join('') + (prompt("Enter anything to break the flow: ").length ? prompt().charCodeAt(0) : 0)).split("").map((num, idx) => String.fromCharCode(num.charCodeAt(0) + idx)).join('')}`; }
+
+
